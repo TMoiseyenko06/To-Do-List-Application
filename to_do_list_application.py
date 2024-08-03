09e9e5bd13241c7ee3b0f3db37b9418aee5fc375
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-08-03 00:05:59 -0400

    Added some funcionality to the create list option but still not fully operational

--- to_do_list_application.py
+++ to_do_list_application.py
@@ -1,12 +1,16 @@
 '''
 To-Do List 
-Version 0.1.0
+Version 0.1.1
 
 A simple to-do list project I did for Coding Temple!
 It prints out a menu of options in the console, select options and enter tasks to use it.
 I'm planning to add more fetures and clean up the code in the future
 
 '''
+#initilize global variables
+
+avalible_lists = [{}]
+current_list = 0
 
 #adds a task to the dictionary
 def add_task(task_list,task):
@@ -32,8 +36,11 @@ def view_tasks(task_list):
     for task in task_list:
         print(f'\t\t{'[x]' if task_list[task] else '[ ]'} : {task}')
 
-def create_list():
-    pass
+def create_list(task):
+    global current_list 
+    avalible_lists.append({})
+    current_list += 1
+    print(f'Created list {current_list + 1}')
         
 #decide what function to run based on the inpu
 def run_choice(task_list,choice,task=""):
@@ -46,7 +53,7 @@ def run_choice(task_list,choice,task=""):
     elif choice == 4:
         pass
     elif choice == 5:
-        pass
+        create_list(task)
     elif choice == 6:
         view_tasks(task_list)
         input("\t\tClick 'Enter' to return to the menu")
@@ -55,34 +62,32 @@ def run_choice(task_list,choice,task=""):
 
 #main function that controls everything else
 def main():
-    task_list = {}
-    Avalible_lists = [task_list]
-    current_list = 0
+
     while True:
         try:
             #ask for user choice
-            choice = int(input("""\n\n\tWelcome to the To-Do list application
-                \n\t\tMenu:
+            choice = int(input(f"""\n\n\tWelcome to the To-Do list application
+                \n\t\tMenu for list {current_list + 1}:
                 1. Add a task
                 2. Delete a Task
                 3. Mark a Task as Complete
-                4. Create New List
-                5. Change current List
+                4. Change current List
+                5. Create new list
                 6. View Tasks
                 7. Quit
                       
                       """))    
-        except:
+        except :
             print("\n\n\tNumber is not vlaid please try again.")
             break
         else:
             #decide if we need to ask user for a task
             if choice < 7:
-                if not choice == 6:
+                if choice <= 4 :
                     task = input("\t\tPlease enter The name of the task:")
-                    run_choice(Avalible_lists[current_list],choice,task)
+                    run_choice(avalible_lists[current_list],choice,task)
                 else:
-                    run_choice(Avalible_lists[current_list],choice)       
+                    run_choice(avalible_lists[current_list],choice)       
             else:
                 print("\t\tHave a great rest of your day!")
                 break
